9595e4616613c6825fa28d2b43b61a09904db46c
Z Yin <Z.Yin.MD@gmail.com>
2025-01-03 23:53:49 -0500

    ready to move to dropbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+  <title>Z Yin</title>
+  <meta name="description" content="Z Yin's resume" />
+</svelte:head>
+
 <!-- @component  -->
 <div class="component">hello world</div>
 
